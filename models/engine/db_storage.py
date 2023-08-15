1dbf2a161dcb3fa8749539c6f67ab89947467ac7
Oscarsa3 <oscarorlandosalinassilvestre@gmail.com>
2023-08-15 15:06:23 -0500

    fix:             self.reload()

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -61,6 +61,7 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute
         (self.__session)"""
-        self.__session.remove()
-        self.__session = None
-        self.reload()
+        if self.__session:
+            self.__session.remove()
+            self.__session = None
+            self.reload()
